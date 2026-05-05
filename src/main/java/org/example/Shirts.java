0faf4bbbc64754f1d61cc7b06545b20a67cedf51
Mykyta Bychenok <mykytabychenok.gmail.com>
2026-05-05 06:00:21 +0300

    Save minor formatting changes

---
+++ src/main/java/org/example/Shirts.java
@@ -0,0 +1,27 @@
+package org.example;
+
+/**
+ * Похідний клас "Сорочки", успадкований від Clothes.
+ */
+public class Shirts extends Clothes {
+    private boolean shortSleeves; // Чи короткі рукави
+
+    public Shirts(String type, String brand, Size size, double price, boolean shortSleeves) {
+        super(type, brand, size, price); // Виклик конструктора базового класу
+        this.shortSleeves = shortSleeves;
+    }
+
+    public boolean isShortSleeves() { return shortSleeves; }
+    public void setShortSleeves(boolean shortSleeves) { this.shortSleeves = shortSleeves; }
+
+    @Override
+    public String toString() {
+        // Перевизначаємо toString для демонстрації поліморфізму
+        return "Shirts{" +
+                "brand='" + getBrand() + '\'' +
+                ", size=" + getSize() +
+                ", price=" + getPrice() +
+                ", shortSleeves=" + shortSleeves +
+                '}';
+    }
+}
---
+++ src/main/java/org/example/Size.java
@@ -0,0 +1,8 @@
+package org.example;
+
+/**
+ * Перерахування, що представляє фіксований набір розмірів одягу.
+ */
+public enum Size {
+    XS, S, M, L, XL, XXL
+}
