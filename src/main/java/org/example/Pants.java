6144dcb3aa378da2693c8a823f7dcd7f85f71ed3
Mykyta Bychenok <mykytabychenok.gmail.com>
2026-05-05 05:43:30 +0300

    Add derived classes Pants and Shirts for base model

---
+++ src/main/java/org/example/Pants.java
@@ -0,0 +1,27 @@
+package org.example;
+
+/**
+ * Похідний клас "Штани", успадкований від Clothes.
+ */
+public class Pants extends Clothes {
+    private boolean hasBelt; // Чи є ремінь у комплекті
+
+    public Pants(String type, String brand, Size size, double price, boolean hasBelt) {
+        super(type, brand, size, price); // Виклик конструктора базового класу
+        this.hasBelt = hasBelt;
+    }
+
+    public boolean isHasBelt() { return hasBelt; }
+    public void setHasBelt(boolean hasBelt) { this.hasBelt = hasBelt; }
+
+    @Override
+    public String toString() {
+        // Перевизначаємо toString для демонстрації поліморфізму
+        return "Pants{" +
+                "brand='" + getBrand() + '\'' +
+                ", size=" + getSize() +
+                ", price=" + getPrice() +
+                ", hasBelt=" + hasBelt +
+                '}';
+    }
+}
