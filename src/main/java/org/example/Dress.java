d4240984508de68409a402d8e2c9fb6a7cda9878
Mykyta Bychenok <mykytabychenok.gmail.com>
2026-05-05 06:05:47 +0300

    Add Dress class to hierarchy

---
+++ src/main/java/org/example/Dress.java
@@ -0,0 +1,27 @@
+package org.example;
+
+/**
+ * Похідний клас "Сукня", успадкований від Clothes.
+ */
+public class Dress extends Clothes {
+    private boolean isEvening; // Чи є вечірньою сукнею
+
+    public Dress(String type, String brand, Size size, double price, boolean isEvening) {
+        super(type, brand, size, price);
+        this.isEvening = isEvening;
+    }
+
+    public boolean isEvening() { return isEvening; }
+    public void setEvening(boolean evening) { this.isEvening = evening; }
+
+    @Override
+    public String toString() {
+        return "Dress{" +
+                "type='" + getType() + '\'' +
+                ", brand='" + getBrand() + '\'' +
+                ", size=" + getSize() +
+                ", price=" + getPrice() +
+                ", isEvening=" + isEvening +
+                '}';
+    }
+}
